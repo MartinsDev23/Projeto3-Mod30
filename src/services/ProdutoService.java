5e12126ec9c738e8c4fa9588890df183212ee546
Daniel Martins <danmartins016@gmail.com>
2025-04-03 20:34:10 -0300

    Versionando o código para correção

---
+++ src/services/ProdutoService.java
@@ -0,0 +1,12 @@
+package services;
+
+import dao.IProdutoDAO;
+import domain.Produto;
+import services.generic.GenericService;
+
+public class ProdutoService extends GenericService<Produto, String> implements IProdutoService {
+
+    public ProdutoService(IProdutoDAO dao) {
+        super(dao);
+    }
+}
